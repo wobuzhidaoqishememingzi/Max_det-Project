d5e10829f058ec955b2387b5bee2d6ed8bf9034b
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-09-24 17:47:56 -0700

    new maxdet.sh

--- maxdet.sh
+++ maxdet.sh
@@ -19,6 +19,9 @@
 # Request RAM (below = 8gb x4 cpu =32gb for the job)
 #SBATCH --mem-per-cpu=2gb
 
+# This will set the SLURM_NTASKS environment variable to "1"
+#SBATCH --ntasks=1
+
 source ~/.bashrc
 conda activate pytorch
 python /home/yuebi/Project/fc_loop.py --exp_name=dim11_run_23 --dump_path=/home/yuebi/Project/dim11_run --num_initial_empty_objects=600 --final_database_size=15 --target_db_size=15 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=131 --sample-only=30000 --max-steps=1000 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128
