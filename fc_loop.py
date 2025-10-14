0689cc87a24785e1503a4e3eaf05770d07e471a7
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-10-14 02:33:24 -0700

    dim15_run

--- fc_loop.py
+++ fc_loop.py
@@ -24,7 +24,7 @@ import argparse
 
 import collections
 
-size = 14 # size of the matrix
+size = 15 # size of the matrix
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
--- maxdet.sh
+++ maxdet.sh
@@ -19,10 +19,12 @@
 # Request RAM (below = 8gb x4 cpu =32gb for the job)
 #SBATCH --mem-per-cpu=8gb
 
+#SBATCH --array=1-2%1
+#SBATCH --dependency=singleton
 
 # This will set the SLURM_NTASKS environment variable to "1"
 #SBATCH --ntasks=1
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim14_run_1 --dump_path=/home/yuebi/Project/dim14_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=209 --sample-only=50000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim15_run_1 --dump_path=/home/yuebi/Project/dim15_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=239 --sample-only=50000 --max-steps=1000 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
