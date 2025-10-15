659a5cc7ac7e2bf2dc9ce400bfcfe5c739bb9529
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-10-15 15:39:47 -0700

    dim16_run_2 using less sample and try a longer run

--- maxdet.sh
+++ maxdet.sh
@@ -19,7 +19,7 @@
 # Request RAM (below = 8gb x4 cpu =32gb for the job)
 #SBATCH --mem-per-cpu=8gb
 
-#SBATCH --array=1-3%1
+#SBATCH --array=1-5%1
 #SBATCH --dependency=singleton
 
 # This will set the SLURM_NTASKS environment variable to "1"
@@ -27,4 +27,4 @@
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim16_run_1 --dump_path=/home/yuebi/Project/dim16_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=271 --sample-only=40000 --max-steps=1000 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim16_run_2 --dump_path=/home/yuebi/Project/dim16_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=271 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
