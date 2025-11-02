9797cea03dbe7e7f05e0b523aa7b42b6fe1cb3ee
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-11-02 01:30:41 -0800

    dim15_run_2

--- maxdet.sh
+++ maxdet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # maxdet.sh
 
-#SBATCH --job-name=maxdet14
+#SBATCH --job-name=maxdet15
 
 #SBATCH --mail-user=ybili@ucdavis.edu
 #SBATCH --mail-type=ALL
@@ -13,7 +13,7 @@
 #SBATCH --gpus=1g.10gb:1
 
 # Request a certain amount of time (hour per epoch)
-#SBATCH --time=03:40:00
+#SBATCH --time=02:30:00
 
 # Request cpus
 #SBATCH --cpus-per-gpu=2
@@ -21,7 +21,7 @@
 # Request RAM (below = 8gb x4 cpu =32gb for the job)
 #SBATCH --mem-per-cpu=8gb
 
-#SBATCH --array=1-10%1
+#SBATCH --array=1-20%1
 #SBATCH --dependency=singleton
 
 # This will set the SLURM_NTASKS environment variable to "1"
@@ -29,4 +29,4 @@
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim14_run_2 --dump_path=/home/yuebi/Project/dim14_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=22 --type=transformer --max-output-length=305 --sample-only=50000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim15_run_2 --dump_path=/home/yuebi/Project/dim15_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=305 --sample-only=50000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
