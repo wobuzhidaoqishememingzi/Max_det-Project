d47b68fa48cbfa3c94654265bb9cc5f33ae2507b
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-11-06 15:56:05 -0800

    dim14_run_3

--- fc_loop.py
+++ fc_loop.py
@@ -24,7 +24,7 @@ import argparse
 
 import collections
 
-size = 17 # size of the matrix
+size = 14 # size of the matrix
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
--- maxdet.sh
+++ maxdet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # maxdet.sh
 
-#SBATCH --job-name=maxdet17
+#SBATCH --job-name=maxdet14
 
 #SBATCH --mail-user=ybili@ucdavis.edu
 #SBATCH --mail-type=ALL
@@ -12,8 +12,8 @@
 # Request a GPU (1x 10gb)
 #SBATCH --gpus=1g.10gb:1
 
-# Request a certain amount of time (hour per epoch)
-#SBATCH --time=03:15:00
+# Request a certain amount of time (hour per two epoch)
+#SBATCH --time=03:45:00
 
 # Request cpus
 #SBATCH --cpus-per-gpu=2
@@ -21,7 +21,7 @@
 # Request RAM (below = 8gb x4 cpu =32gb for the job)
 #SBATCH --mem-per-cpu=8gb
 
-#SBATCH --array=1-15%1
+#SBATCH --array=1-10%1
 #SBATCH --dependency=singleton
 
 # This will set the SLURM_NTASKS environment variable to "1"
@@ -29,4 +29,4 @@
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim17_run_5 --dump_path=/home/yuebi/Project/dim17_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=305 --sample-only=30000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim14_run_3 --dump_path=/home/yuebi/Project/dim14_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=22 --type=transformer --max-output-length=209 --sample-only=50000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
