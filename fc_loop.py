56388d08107f53353374a847615e6c68ac239530
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-11-17 15:51:14 -0800

    dim19_run_1

--- fc_loop.py
+++ fc_loop.py
@@ -24,7 +24,7 @@ import argparse
 
 import collections
 
-size = 18 # size of the matrix
+size = 19 # size of the matrix
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
--- maxdet.sh
+++ maxdet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # maxdet.sh
 
-#SBATCH --job-name=maxdet18
+#SBATCH --job-name=maxdet19
 
 #SBATCH --mail-user=ybili@ucdavis.edu
 #SBATCH --mail-type=ALL
@@ -29,4 +29,4 @@
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim18_run_1 --dump_path=/home/yuebi/Project/dim18_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=305 --sample-only=30000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim19_run_1 --dump_path=/home/yuebi/Project/dim19_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=330 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
