37941973af3138fab0ac45e1b47756ac76553ac7
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-12-05 22:01:51 -0800

    dim20_run_3 with the second highest matrix

--- maxdet.sh
+++ maxdet.sh
@@ -13,7 +13,7 @@
 #SBATCH --gpus=1g.20gb:1
 
 # Request a certain amount of time (hour per epoch)
-#SBATCH --time=03:30:00
+#SBATCH --time=03:15:00
 
 # Request cpus
 #SBATCH --cpus-per-gpu=2
@@ -21,7 +21,7 @@
 # Request RAM (below = 8gb x2 cpu =16gb for the job)
 #SBATCH --mem-per-cpu=8gb
 
-#SBATCH --array=1-13%1
+#SBATCH --array=1-20%1
 #SBATCH --dependency=singleton
 
 # This will set the SLURM_NTASKS environment variable to "1"
@@ -29,4 +29,4 @@
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim20_run_2 --dump_path=/home/yuebi/Project/dim20_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=350 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim20_run_3 --dump_path=/home/yuebi/Project/dim20_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=350 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
