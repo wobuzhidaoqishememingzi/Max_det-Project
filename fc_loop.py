9c9496326260e6a221932d647dd64be8605b5831
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-12-06 02:18:46 -0800

    dim22_run with the second highest matrix

--- fc_loop.py
+++ fc_loop.py
@@ -24,7 +24,7 @@ import argparse
 
 import collections
 
-size = 21 # size of the matrix
+size = 22 # size of the matrix
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
--- maxdet.sh
+++ maxdet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # maxdet.sh
 
-#SBATCH --job-name=maxdet21
+#SBATCH --job-name=maxdet22
 
 #SBATCH --mail-user=ybili@ucdavis.edu
 #SBATCH --mail-type=ALL
@@ -13,7 +13,7 @@
 #SBATCH --gpus=1g.20gb:1
 
 # Request a certain amount of time (hour per epoch)
-#SBATCH --time=03:30:00
+#SBATCH --time=03:20:00
 
 # Request cpus
 #SBATCH --cpus-per-gpu=2
@@ -29,4 +29,4 @@
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim21_run_1 --dump_path=/home/yuebi/Project/dim21_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=350 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim22_run_1 --dump_path=/home/yuebi/Project/dim22_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=350 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
