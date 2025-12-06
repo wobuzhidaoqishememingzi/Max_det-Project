a26ed25e7d85a87aed5b7a6425f378ee814f87b1
wobuzhidaoqishememingzi <ybili@ucdavis.edu>
2025-12-06 15:47:20 -0800

    dim24_run with double-flip matrix added

--- fc_loop.py
+++ fc_loop.py
@@ -24,7 +24,7 @@ import argparse
 
 import collections
 
-size = 22 # size of the matrix
+size = 24 # size of the matrix
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
--- maxdet.sh
+++ maxdet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # maxdet.sh
 
-#SBATCH --job-name=maxdet22
+#SBATCH --job-name=maxdet24
 
 #SBATCH --mail-user=ybili@ucdavis.edu
 #SBATCH --mail-type=ALL
@@ -29,4 +29,4 @@
 
 source ~/.bashrc
 conda activate pytorch
-python fc_loop.py --exp_name=dim22_run_1 --dump_path=/home/yuebi/Project/dim22_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=800 --max_epochs=20 --type=transformer --max-output-length=350 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
+python fc_loop.py --exp_name=dim24_run_1 --dump_path=/home/yuebi/Project/dim24_run --num_initial_empty_objects=800 --final_database_size=100 --target_db_size=100 --nb_local_searches=1000 --max_epochs=20 --type=transformer --max-output-length=350 --sample-only=25000 --max-steps=1500 --n_tokens=15 --n-layer=8 --n-embd=128 --n-embd2=128 --exp_id=1111
